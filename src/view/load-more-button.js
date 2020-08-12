d2a590174b1f62738a2108e9b0958006638dab11
Vadim Kornilov <kornilow.home@gmail.com>
2020-08-13 01:32:25 +0700

    Module 4 - Task 1

--- src/view/load-more-button.js
+++ src/view/load-more-button.js
@@ -1,3 +1,26 @@
+import {createElement} from "../utils";
+
 export const createLoadMoreButtonTemplate = () => {
   return `<button class="load-more" type="button">load more</button>`;
 };
+
+export default class LoadMoreButton {
+  constructor(element) {
+    this._element = element;
+  }
+
+  _getTemplate() {
+    return createLoadMoreButtonTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this._getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
